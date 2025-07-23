302aa3f68819102e371d7d5115311e540c237231
Gustavo Gomes <ggs.gustavo.dev@gmail.com>
2025-07-22 21:34:24 -0300

    Update

--- Carros/app/settings.py
+++ Carros/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cars',  # Custom app for car management
 ]
 
 MIDDLEWARE = [
