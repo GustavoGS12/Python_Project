7751a347f8dff2c7fc2290352b086d3c0bb2fefe
Gustavo Gomes <ggs.gustavo.dev@gmail.com>
2025-07-27 20:20:50 -0300

    Update
    Atualização no BD

--- Carros/cars/models.py
+++ Carros/cars/models.py
@@ -6,11 +6,11 @@ class Car(models.Model):
     brand = models.CharField(max_length=200)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
-    value = models.FloatField(max_length=10, decimal_places=2,  blank=True, null=True)
+    value = models.FloatField(max_length=10,  blank=True, null=True)
     color = models.CharField(max_length=50, blank=True, null=True)
     fuel_type = models.CharField(max_length=50, blank=True, null=True)
     transmission = models.CharField(max_length=50, blank=True, null=True)
     mileage = models.FloatField(max_length=7, blank=True, null=True)
-    doors = models.IntegerField(max_length=2, blank=True, null=True)
+    doors = models.IntegerField( blank=True, null=True)
     
     
